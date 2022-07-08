42feee91c93b13ea638b16c4ce4d0544542a1a4b
DJSekora <DJSekora@gmail.com>
2022-07-07 22:05:19 -0400

    Add files via upload

---
+++ Examples/DataStructures/SortsExample.java
@@ -0,0 +1,24 @@
+/* This class includes examples of several sorting algorithms. */
+public class SortsExample
+{
+  public static void bubbleSort(int[] list)
+  {
+  }
+  
+  public static void selectionSort(int[] list)
+  {
+  }
+  
+  public static void insertionSort(int[] list)
+  {
+  }
+  
+  public static void bogoSort(int[] list)
+  {
+  }
+  
+  public static void mergeSort(int[] list)
+  {
+    
+  }
+}
