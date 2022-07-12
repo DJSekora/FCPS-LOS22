3a77564fd08b09a69b70b6593a5af7477f398ada
DJSekora <DJSekora@gmail.com>
2022-07-12 12:13:13 -0400

    Add files via upload

---
+++ Examples/Graphics/MouseExample.java
@@ -0,0 +1,66 @@
+/* This class demonstrates how to use the mouse to interact with your JPanel.
+   As always, the specific syntax here is restricted to Java (and, more
+   particularly, the Swing library), but 
+ */
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+
+// New imports for Mouse
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseExample extends JPanel implements MouseListener
+{
+  Color color = Color.BLACK;
+
+  public MouseExample()
+  {
+    // MouseListener instead of KeyListener
+    addMouseListener(this);
+    setFocusable(true);
+  }
+  
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    
+    g.setColor(color);
+    g.fillRect(300,200,200,200);
+  }
+  
+  /* Required methods for MouseListener */
+  public void mousePressed(MouseEvent e)
+  {
+    // getButton tells us which button was clicked.
+    int button = e.getButton();
+    
+    // BUTTON1 = left click
+    if(button == MouseEvent.BUTTON1)
+    {
+      color = Color.RED;
+    }
+    else if(button == MouseEvent.BUTTON2)
+    {
+      color = Color.GREEN;
+    }
+    else if(button == MouseEvent.BUTTON3)
+    {
+      color = Color.BLUE;
+    }
+    repaint();
+  }
+  public void mouseReleased(MouseEvent e)
+  {
+  }
+  public void mouseClicked(MouseEvent e)
+  {
+  }  
+  public void mouseEntered(MouseEvent e)
+  { 
+  } 
+  public void mouseExited(MouseEvent e)
+  { 
+  } 
+}
